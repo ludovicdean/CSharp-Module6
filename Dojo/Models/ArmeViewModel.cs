d3f94d0f593457b1b225d732f115e1677ebfa77e
Ludo <ludovicd58@gmail.com>
2020-09-21 22:47:30 +0200

    Ajout ViewModels pour Samourai et Arme

---
+++ Dojo/Models/ArmeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dojo.Models
+{
+    public class ArmeViewModel
+    {
+        public Arme Arme { get; set; }
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Degats { get; set; }
+    }
+}
---
+++ Dojo/Models/SamouraiViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dojo.Models
+{
+    public class SamouraiViewModel
+    {
+        public Samourai Samourai { get; set; }
+        public int Id { get; set; }
+        public int Force { get; set; }
+        public string Nom { get; set; }
+        public List<SelectListItem> Armes { get; set; } = new List<SelectListItem>();
+    }
+}
